d57ecf38cea8fe9833be2fb24cd6756bdc5af976
ane-k-goncalves <goncalvesanne961@gmail.com>
2025-05-17 09:15:42 -0300

    components card-categoria, card-produtos e novas paginas

--- glow-essence/src/app/app.routes.ts
+++ glow-essence/src/app/app.routes.ts
@@ -3,18 +3,33 @@ import { CadastroComponent } from './components/cadastro/cadastro.component';
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { CarrinhoComponent } from './pages/carrinho/carrinho.component';
+import { FavoritosComponent } from './pages/favoritos/favoritos.component';
+import { PerfilComponent } from './pages/perfil/perfil.component';
 
 export const routes: Routes = [
-    {
-        path: "app-cadastro",
-        component: CadastroComponent
-    },
-   // {
-     //   path: "",
-       // component: LoginComponent
-    //},
-      {
-        path: "",
-        component: HomeComponent
-    }
-]
+  {
+    path: 'app-cadastro',
+    component: CadastroComponent,
+  },
+  // {
+  //   path: "",
+  // component: LoginComponent
+  //},
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'app-carrinho',
+    component: CarrinhoComponent,
+  },
+  {
+    path: 'app-favoritos',
+    component: FavoritosComponent,
+  },
+  {
+    path: 'app-perfil',
+    component: PerfilComponent,
+  },
+];
