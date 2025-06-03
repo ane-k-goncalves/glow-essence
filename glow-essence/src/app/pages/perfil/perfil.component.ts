34b22bd9adf34e0aaaad1247243abea4c6f51542
ane-k-goncalves <goncalvesanne961@gmail.com>
2025-06-02 22:34:37 -0300

    page perfil

--- glow-essence/src/app/pages/perfil/perfil.component.ts
+++ glow-essence/src/app/pages/perfil/perfil.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { User, UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-perfil',
-  imports: [],
+  imports: [NavbarComponent, FooterComponent ],
   templateUrl: './perfil.component.html',
   styleUrl: './perfil.component.css'
 })
-export class PerfilComponent {
+export class PerfilComponent  implements OnInit {
+ @Input() user!: null | User; 
 
+ 
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.user = this.userService.getLoggedUser();
+  }
 }
