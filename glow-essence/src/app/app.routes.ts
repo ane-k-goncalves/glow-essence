801ebba93b26c45aff2919be19b2d0490b5ccdad
Ane Karine Goncalves <ane.goncalves@superpao.com.br>
2025-07-31 17:42:11 -0300

    alteracoes

--- glow-essence/src/app/app.routes.ts
+++ glow-essence/src/app/app.routes.ts
@@ -3,8 +3,6 @@ import { CadastroComponent } from './components/cadastro/cadastro.component';
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
-import { CarrinhoComponent } from './pages/carrinho/carrinho.component';
-import { FavoritosComponent } from './pages/favoritos/favoritos.component';
 import { PerfilComponent } from './pages/perfil/perfil.component';
 
 export const routes: Routes = [
@@ -20,14 +18,8 @@ export const routes: Routes = [
     path: 'app-home',
     component: HomeComponent,
   },
-  {
-    path: 'app-carrinho',
-    component: CarrinhoComponent,
-  },
-  {
-    path: 'app-favoritos',
-    component: FavoritosComponent,
-  },
+  
+  
   {
     path: 'app-perfil',
     component: PerfilComponent,
