77547c58554d7c4956df0ed9dfa509a6faafbb3e
ane-k-goncalves <goncalvesanne961@gmail.com>
2025-08-10 15:14:59 -0300

    Alterações tema projeto

---
+++ glow-essence/src/app/components/modal-cartoes/modal-cartoes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-cartoes',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal-cartoes.component.html',
+  styleUrl: './modal-cartoes.component.css',
+})
+export class ModalCartoesComponent {}
